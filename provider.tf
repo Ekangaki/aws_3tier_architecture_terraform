dc14cf809c35b7690bbe6487688f15bf7434b461
E Njang <georgegedeon2012@gmail.com>
2025-01-02 10:23:44 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,11 +6,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.20.1"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
